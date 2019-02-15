002413b9e9c4015963c44eb37abb76a091a63b4a
Konstantinos Pittas <konstantinos.pittas@gmail.com>
2019-02-15 12:32:25 +0200

    Create an abstract command

---
+++ src/Console/Command.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace PhpNsFixer\Console;
+
+use PhpNsFixer\Event\FileProcessedEvent;
+use PhpNsFixer\Fixer\Result;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Tightenco\Collect\Support\Collection;
+
+abstract class Command extends SymfonyCommand
+{
+    /**
+     * @var EventDispatcher
+     */
+    protected $dispatcher;
+
+    /**
+     * @var ProgressBar
+     */
+    protected $progressBar;
+
+    public function __construct($name = null)
+    {
+        parent::__construct($name);
+
+        $this->dispatcher = new EventDispatcher();
+        $this->dispatcher->addListener(FileProcessedEvent::class, function (FileProcessedEvent $event) {
+            $this->progressBar->setMessage($event->getFile()->getRelativePathname(), 'filename');
+            $this->progressBar->advance();
+        });
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param Collection $files
+     * @return void
+     */
+    protected function progressStart(OutputInterface $output, Collection $files): void
+    {
+        ProgressBar::setFormatDefinition('custom', 'Checking files... %current%/%max% (%filename%)');
+
+        $this->progressBar = new ProgressBar($output, $files->count());
+        $this->progressBar->setFormat('custom');
+        $this->progressBar->start();
+    }
+
+    /**
+     * @param OutputInterface $output
+     */
+    protected function progressFinish(OutputInterface $output): void
+    {
+        $this->progressBar->setMessage('Done', 'filename');
+        $this->progressBar->finish();
+
+        $output->writeln("\n");
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param Collection $problematicFiles
+     * @param bool $dryRun
+     */
+    protected function printResults(OutputInterface $output, Collection $problematicFiles, bool $dryRun): void
+    {
+        if ($problematicFiles->count() === 0) {
+            $output->writeln("<info>No problems found! :)</info>");
+            return;
+        }
+
+        $output->writeln(
+            sprintf(
+                "<options=bold,underscore>There %s %d wrong %s:</>\n",
+                $this->verbForMessage($problematicFiles, $dryRun),
+                $problematicFiles->count(),
+                $problematicFiles->count() !== 1 ? 'namespaces' : 'namespace'
+            )
+        );
+
+        $problematicFiles->each(function (Result $result, $key) use ($output) {
+            $output->writeln(sprintf("%d) %s:", $key + 1, $result->getFile()->getRelativePathname()));
+            $output->writeln(sprintf("\t<fg=red>- %s</>", $result->getExpected()));
+            $output->writeln(sprintf("\t<fg=green>+ %s</>", $result->getActual()));
+        });
+    }
+
+    /**
+     * @param Collection $problematicFiles
+     * @param bool $isDryRun
+     * @return string
+     */
+    protected function verbForMessage(Collection $problematicFiles, bool $isDryRun = false): string
+    {
+        return $problematicFiles->count() !== 1 ? ($isDryRun ? 'are' : 'were') : ($isDryRun ? 'is' : 'was');
+    }
+}
--- src/Console/FixCommand.php
+++ src/Console/FixCommand.php
@@ -4,43 +4,16 @@ declare(strict_types=1);
 
 namespace PhpNsFixer\Console;
 
-use PhpNsFixer\Event\FileProcessedEvent;
 use PhpNsFixer\Finder\FileFinder;
-use PhpNsFixer\Fixer\Result;
 use PhpNsFixer\Runner\Runner;
 use PhpNsFixer\Runner\RunnerOptions;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Tightenco\Collect\Support\Collection;
 
 final class FixCommand extends Command
 {
-    /**
-     * @var ProgressBar
-     */
-    private $progressBar;
-
-    /**
-     * @var EventDispatcher
-     */
-    private $dispatcher;
-
-    public function __construct($name = null)
-    {
-        parent::__construct($name);
-
-        $this->dispatcher = new EventDispatcher();
-        $this->dispatcher->addListener(FileProcessedEvent::class, function (FileProcessedEvent $event) {
-            $this->progressBar->setMessage($event->getFile()->getRelativePathname(), 'filename');
-            $this->progressBar->advance();
-        });
-    }
-
     /**
      * Configures the current command.
      *
@@ -76,61 +49,8 @@ final class FixCommand extends Command
 
         $this->progressFinish($output);
 
-        if ($problematicFiles->count() === 0) {
-            $output->writeln("<info>No problems found! :)</info>");
-            return 0;
-        }
-
-        $output->writeln(
-            sprintf(
-                "<options=bold,underscore>There %s %d wrong %s:</>\n",
-                $this->verbForMessage($problematicFiles, $dryRun),
-                $problematicFiles->count(),
-                $problematicFiles->count() !== 1 ? 'namespaces' : 'namespace'
-            )
-        );
-
-        $problematicFiles->each(function (Result $result, $key) use ($output) {
-            $output->writeln(sprintf("%d) %s:", $key + 1, $result->getFile()->getRelativePathname()));
-            $output->writeln(sprintf("\t<fg=red>- %s</>", $result->getExpected()));
-            $output->writeln(sprintf("\t<fg=green>+ %s</>", $result->getActual()));
-        });
+        $this->printResults($output, $problematicFiles, $dryRun);
 
         return $problematicFiles->count();
-    }
-
-    /**
-     * @param OutputInterface $output
-     * @param Collection $files
-     * @return void
-     */
-    private function progressStart(OutputInterface $output, Collection $files): void
-    {
-        ProgressBar::setFormatDefinition('custom', 'Checking files... %current%/%max% (%filename%)');
-
-        $this->progressBar = new ProgressBar($output, $files->count());
-        $this->progressBar->setFormat('custom');
-        $this->progressBar->start();
-    }
-
-    /**
-     * @param OutputInterface $output
-     */
-    private function progressFinish(OutputInterface $output): void
-    {
-        $this->progressBar->setMessage('Done', 'filename');
-        $this->progressBar->finish();
-
-        $output->writeln("\n");
-    }
-
-    /**
-     * @param Collection $problematicFiles
-     * @param bool $isDryRun
-     * @return string
-     */
-    private function verbForMessage(Collection $problematicFiles, bool $isDryRun = false): string
-    {
-        return $problematicFiles->count() !== 1 ? ($isDryRun ? 'are' : 'were') : ($isDryRun ? 'is' : 'was');
     }
 }
